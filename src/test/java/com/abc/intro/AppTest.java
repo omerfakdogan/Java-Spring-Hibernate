9e6cd9ba89a78512ea48b8a8b985f04419b1518d
Ömer Faruk Akdoğan <ofa10745@gmail.com>
2023-10-17 13:36:58 +0300

    Java-Intro

---
+++ src/test/java/com/abc/intro/AppTest.java
@@ -0,0 +1,13 @@
+package com.abc.intro;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
